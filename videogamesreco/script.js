85828263d85274ddeec56b4fb2b8aa219cd2c950
miguel angel <miguelangelcamposhipolito@gmail.com>
2026-08-21 19:07:43 -0600

    agrege css y js

---
+++ videogamesreco/script.js
@@ -0,0 +1,5 @@
+const juegoFavorito = "Hollow Knight";
+const puntuacion = 9;
+let mensaje = "Mi juego favorito es: " + juegoFavorito;
+console.log(mensaje);
+console.log("Puntuación personal: " + puntuacion + "/10");
